8317925316357629fc251237f8c111e4c17db393
cristianpb <felipebrokate@gmail.com>
2018-08-09 00:59:10 +0200

    Heroku conf params

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ mongoose.connect(`mongodb://${mlab_username}:${mlab_password}@ds111192.mlab.com:
 app.use(require('cors')());
 app.use(require('body-parser').json());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/myApp')));
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.sendFile(path.join(__dirname, 'dist/myApp/index.html'))
 });
 
 app.get('/api/tweets/:page', (req, res) => {
