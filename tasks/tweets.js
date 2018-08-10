02bdb65948481dbcc55b0c0ef4ebe3dc3de7f800
cristianpb <felipebrokate@gmail.com>
2018-08-10 12:36:43 +0200

    Set a try/catch for mongo connection

--- tasks/tweets.js
+++ tasks/tweets.js
@@ -17,68 +17,88 @@ var url = `mongodb://${mlab_username}:${mlab_password}@ds111192.mlab.com:11192/n
 
 var Tweet = require('../models/Tweet');
 
-var stream = T.stream('statuses/filter', {
-  track: ["#paris", "#lapaz", "#hongkong", "#sydney", "#bruxelles", "#carthage", "#douala", "#lima", "#istanbul", "#taipei", "#mexico"]
-});
+MongoClient.connect(url, { useNewUrlParser: true })
+  .then(client => {
+    var db = client.db('ng-tweets')
 
-stream.on('tweet', function (tweet) {
-  if ('retweeted_status' in tweet) {
-    //var tweetEntry = new Tweet(tweet.retweeted_status);
-    saveTweets(url, tweet.retweeted_status)
-  } else {
-    //var tweetEntry = new Tweet(tweet);
-    saveTweets(url, tweet)
-  }
-})
 
-stream.on('limit', function (limitMessage) {
-  console.log('Limit for User : on query has rechead!');
-});
+    var stream = T.stream('statuses/filter', {
+      track: ["#paris", "#lapaz", "#hongkong", "#sydney", "#bruxelles", "#carthage", "#douala", "#lima", "#istanbul", "#taipei", "#mexico"]
+    });
 
-stream.on('warning', function (warning) {
-  console.log('warning', warning);
-});
 
-// https://dev.twitter.com/streaming/overview/connecting
-stream.on('reconnect', function (request, response, connectInterval) {
-  console.log('reconnect :: connectInterval', connectInterval)
-});
 
-stream.on('disconnect', function (disconnectMessage) {
-  console.log('disconnect', disconnectMessage);
-});
-
-async function saveTweets (url, tweet) {
-  //console.log(moment('Mon Aug 06 12:04:08 +0000 2018','ddd MMM DD HH:mm:ss Z YYYY').valueOf())
-  tweet = await Object.assign(tweet, {'timestamp_ms': moment(tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').valueOf()});
-  let client = await MongoClient.connect(url, { useNewUrlParser: true,  wtimeout: 0 }).catch( async function(err) {
-    console.log('Error: ');
-    console.log(err)
-    await sleep(5000);
-    console.log('Trying to reconnect');
-    let client = await MongoClient.connect(url, { useNewUrlParser: true,  wtimeout: 0 })
-    return client
-  });
-  console.time(colors.magenta(tweet.id_str));
-  const db = await client.db('ng-tweets')
-  const collection = await db.collection('tweets')
-  collection.insertOne(tweet)
-    .then(saveMetadata(tweet, db))
-    .catch(err => {
-      console.log(err);
+    stream.on('tweet', function (tweet) {
+      let err, msg;
+      console.time(colors.magenta(tweet.id_str));
+      if ('retweeted_status' in tweet) {
+        //var tweetEntry = new Tweet(tweet.retweeted_status);
+        saveTweets(db, tweet.retweeted_status, function(err, msg) {
+          if (err) console.log(err);
+          console.timeEnd(colors.magenta(tweet.id_str));
+        })
+      } else {
+        //var tweetEntry = new Tweet(tweet);
+        saveTweets(db, tweet, function (err, msg) {
+          if (err) console.log(err);
+          console.timeEnd(colors.magenta(tweet.id_str));
+        })
+      }
     })
+
+    stream.on('limit', function (limitMessage) {
+      console.log('Limit for User : on query has rechead!');
+    });
+
+    stream.on('warning', function (warning) {
+      console.log('warning', warning);
+    });
+
+    // https://dev.twitter.com/streaming/overview/connecting
+    stream.on('reconnect', function (request, response, connectInterval) {
+      console.log('reconnect :: connectInterval', connectInterval)
+    });
+
+    stream.on('disconnect', function (disconnectMessage) {
+      client.close()
+      console.log('disconnect', disconnectMessage);
+    });
+
+  });
+
+async function saveTweets (db, tweet, callback) {
+  tweet = await Object.assign(tweet, {'timestamp_ms': moment(tweet.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').valueOf()});
+  try {
+    const collection = await db.collection('tweets')
+    await collection.insertOne(tweet)
+      .then(saveMetadata(db, tweet))
+      .catch(err => {
+        if (err.code !== 11000) console.log(err);
+      })
+    callback(null, `Saved ${tweet.id_str}`)
+  } catch(err) {
+    console.log('Error: ');
+    return callback(err)
+  };
+
 }
 
 
-async function saveHashtag (tweet, reg, type, db) {
+async function saveHashtag (db, tweet, reg, type) {
   let text = await ('extended_tweet' in tweet) ? tweet.extended_tweet.full_text : tweet.text
   let hashtags = await text.match(reg);
-  console.log(text);
-  console.log(hashtags);
   if (hashtags) {
+    console.log('Text: ', text, '\n', type, ': ', hashtags);
     if (hashtags.length > 0) {
+      await db.collection('tweets').findOneAndUpdate(
+        {"id_str": tweet.id_str},
+        { $set: { "hashtags": hashtags }}
+      ).catch(err => console.log('Tweet update error', err))
+      console.log(`updated tweet ${tweet.id_str}`)
       hashtags.forEach(tag => processHashtag(db, tag, hashtags, type, tweet))
     }
+  } else {
+    console.log(colors.red('Text: ', text, '\nNo ', type));
   }
 }
 
@@ -95,21 +115,16 @@ async function processHashtag (db, tag, hashtags, type, tweet) {
       { 'label': norm_tag, 'type': type },
       { $inc: { "value" : 1 }}
     ).catch(err => console.log('Hash update error', err))
-    console.log('updated hashtag')
+    console.log('updated hashtag: ', norm_tag)
   }
-    await db.collection('tweets').findOneAndUpdate(
-      {"id_str": tweet.id_str},
-      { $set: { "hashtags": hashtags }}
-    ).catch(err => console.log('Tweet update error', err))
-    console.log('updated tweet')
 }
 
-async function saveMetadata (tweet, db) {
-  const msg3 = await saveHashtag(tweet, /\B(\#[a-zA-Z0-9]+\b)(?!;)/gm, 'hashtag', db)
-  const msg4 = await saveHashtag(tweet, /\B(\@[a-zA-Z0-9]+\b)(?!;)/gm, 'user_count', db)
-  console.timeEnd(colors.magenta(tweet.id_str));
+async function saveMetadata (db, tweet) {
+  const msg3 = await saveHashtag(db, tweet, /\B(\#[a-zA-Z0-9]+\b)(?!;)/gm, 'hashtag')
+  const msg4 = await saveHashtag(db, tweet, /\B(\@[a-zA-Z0-9]+\b)(?!;)/gm, 'mention')
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
