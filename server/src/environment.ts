745f30da22b5d1daaaaa76aa9e06bb00109172b3
cristianpb <felipebrokate@gmail.com>
2018-10-27 09:56:11 +0200

    Use production environement by default

--- server/src/environment.ts
+++ server/src/environment.ts
@@ -2,6 +2,6 @@ const mlab_username = process.env.MLAB_USERNAME;
 const mlab_password = process.env.MLAB_PASSWORD;
 
 export const environment = {
-  // mongourl = `mongodb://${mlab_username}:${mlab_password}@ds111192.mlab.com:11192/ng-tweets`
-  mongourl: 'mongodb://localhost:27017/ng-tweets'
+  mongourl:  `mongodb://${mlab_username}:${mlab_password}@ds111192.mlab.com:11192/ng-tweets`
+  //mongourl: 'mongodb://localhost:27017/ng-tweets'
 };
